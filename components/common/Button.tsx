7a0dd62e149102c7c9bc69936cb8db0e04661a80
Orion <Orion45@dd.gmail.com>
2025-07-06 19:52:45 +0200

    reusable Card component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -5,9 +5,10 @@ interface C_Button {
     onClick: () => void;
 }
 
-export const Button: React.FC<C_Button> = ({ label, onClick }) => {
+export const Button = ({ label, onClick }: C_Button) => {
     <button onClick={onClick} 
-    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">{label}</button>;
+    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+        {label}</button>;
 };
 
 export default Button;
--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 interface Card_info {
-    title: string;
     image: string;
+    title: string;   
     description: string;
 }
 
